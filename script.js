2fa1a1b7f49c48d40c8542138d9eb2414bcea3df
tunga2cvp <thanhtunga2cvp@gmail.com>
2021-05-14 21:02:27 +0700

    admin page

--- script.js
+++ script.js
@@ -3,12 +3,13 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
-function myFunction(){
+function editorChanged(){
     var language = document.getElementById("language-selector").value;
     var editorNumber = document.getElementsByClassName("editor").length;
     var i;
     for ( i=0 ; i< editorNumber; i++){
         document.getElementsByClassName("editor")[i].style.display = "none";
+        document.getElementsByClassName("alone-editor")[i].style.display = "none";
     }
     switch(language){
         case "Web Development":
@@ -28,3 +29,57 @@ function myFunction(){
         default:
     }
 }
+function showConsole(){
+    if ( document.getElementById("console").style.display === "block")
+        document.getElementById("console").style.display = "none";
+    else document.getElementById("console").style.display = "block";
+}
+
+document.getElementById('textbox').addEventListener('keydown', function(e) {
+    if (e.key == 'Tab') {
+      e.preventDefault();
+      var start = this.selectionStart;
+      var end = this.selectionEnd;
+  
+      // set textarea value to: text before caret + tab + text after caret
+      this.value = this.value.substring(0, start) +
+        "\t" + this.value.substring(end);
+  
+      // put caret at right position again
+      this.selectionStart =
+        this.selectionEnd = start + 1;
+    }
+  });
+// script for admin page
+
+  function navController(){
+      if ( document.getElementById("mySidenav").style.width == "0px" ){
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("content-wrapper").style.marginLeft = "250px";
+      }
+      else {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("content-wrapper").style.marginLeft = "0px";
+      }
+  }
+  function fileManagerBtnHandler(){
+    if ( document.getElementById("file-manager-dropdown").style.display === "block" )
+        document.getElementById("file-manager-dropdown").style.display = "none";
+    else document.getElementById("file-manager-dropdown").style.display = "block";
+  }
+
+  function showPostManager(){
+    document.getElementById("dashboard").style.display = "none";
+    document.getElementById("post-manager").style.display = "block";
+    document.getElementById("excercise-manager").style.display = "none";
+  }
+  function showDashboard(){
+    document.getElementById("dashboard").style.display = "block";
+    document.getElementById("post-manager").style.display = "none";
+    document.getElementById("excercise-manager").style.display = "none";
+  }
+  function showExcerciseManager(){
+    document.getElementById("dashboard").style.display = "none";
+    document.getElementById("post-manager").style.display = "none";
+    document.getElementById("excercise-manager").style.display = "block";
+  }
